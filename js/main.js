25f9dc7059b2fe795a7a06c285fd0f685d6dda0b
hevoren <fifaultimateamfifa15@gmail.com>
2023-03-14 11:41:00 +0700

    full remake

--- js/main.js
+++ js/main.js
@@ -1,55 +1,47 @@
 let eventBus = new Vue()
 
-Vue.component('notes', {
-       template:`
-        <div class="notes">
-            <create-note></create-note>
-            <div class="note-list">
-                <first-note></first-note>
-                <second-note></second-note>
-                <third-note></third-note>
+Vue.component('all-notes', {
+    template:`
+            <div class="all-notes">
+                <create-note></create-note>
+                <notes></notes>
             </div>
-        </div>
     `,
 })
 
-Vue.component('first-note', {
+Vue.component('notes', {
     template: `
-        <div class="note" >
-            <h1 v-for="error in errors"> {{ error }}</h1>
-            <ul>
-                <li v-for="note in column_1" class="li-list">
-                    <h1>{{ note.name }}</h1>
-                    <h3>{{ column_1 }}</h3>
-                    <h3>{{ note }}</h3>
-                    <h3>{{ note.tasks }}</h3>
-                    <h3>{{ note.tasks.name }}</h3>
-                    <ul>
-                        <li v-for="task in note.tasks" class="li-task" v-if="task !== null" >
-                            <p>{{task}}</p>
-                            <input type="checkbox">
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+        <div class="notes">
+            <note :column="column_1" :errors="errors"></note>
+            <note :column="column_2"></note>
+            <note :column="column_3"></note>
         </div>
     `,
 
-    data(){
-        return{
+    data() {
+        return {
             column_1: [],
-            errors: [],
-            active: 0
+            column_2: [
+                {
+                    name: 'garry',
+                    tasks: {
+                        task_1: '123',
+                        task_2: 'sdfgsdf',
+                        task_3: 'gandom',
+                    },
+                    data: null
+            }],
+            column_3: [],
+            errors: []
         }
-
     },
     // mounted - вызывается после того, как компонент был добавлен в DOM, т.е. србатывает после того как даннные улетели из формы сюда.
     // после чего он пытается достать и разобрать строку json из localstorage, и если ее нет, присваивает пустой массив
-
     mounted(){
         this.column_1 = JSON.parse(localStorage.getItem("column_1")) || [];
+        this.column_2 = JSON.parse(localStorage.getItem("column_2")) || [];
+        this.column_3 = JSON.parse(localStorage.getItem("column_3")) || [];
         eventBus.$on('notes-submitted', note => {
-
             this.errors = []
             if (this.column_1.length < 3){
                 this.column_1.push(note);
@@ -57,14 +49,19 @@ Vue.component('first-note', {
             } else {
                 this.errors.push('Maximum count of tasks!');
             }
+
         })
     },
-
     // watch отслеживает изменения, если они есть, то он присваивает и сохраняет новые значения, добавляя их в localstorage и преобразовывая ('stringify') в json формат
-
     watch: {
         column_1(newValue) {
             localStorage.setItem("column_1", JSON.stringify(newValue));
+        },
+        column_2(newValue) {
+            localStorage.setItem("column_2", JSON.stringify(newValue));
+        },
+        column_3(newValue) {
+            localStorage.setItem("column_3", JSON.stringify(newValue));
         },
     },
     // saveNote вызывается после выполнения mounted; присваивает и сохраняет значения в localstorage, преобразовывая ('stringify') их в json формат
@@ -72,116 +69,80 @@ Vue.component('first-note', {
         saveNote_1(){
             localStorage.setItem('column_1', JSON.stringify(this.column_1));
         },
-
-    }
-
-})
-
-Vue.component('second-note', {
-    template: `
-        <div class="note">
-            <ul>
-                <li v-for="note in column_2">
-                    <p>{{ note.name }}</p>
-                    <ul>
-                        <li v-for="task in note.tasks" v-if="task.name !== null">
-                            <p>{{task.name}}</p>
-                            <input>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    `,
-
-    data(){
-        return{
-            column_2: [],
-            errors: [],
-            active: 0
-        }
-    },
-
-    mounted() {
-        this.column_2 = JSON.parse(localStorage.getItem("column_2")) || [];
-    },
-
-    watch: {
-        column_2(newValue) {
-            localStorage.setItem("column_2", JSON.stringify(newValue));
-        },
-    },
-
-    methods: {
         saveNote_2(){
             localStorage.setItem('column_2', JSON.stringify(this.column_2));
         },
-    }
-
-})
-
-Vue.component('third-note', {
-    note:{
-        name:{
-            type: Text,
-            required: true
-        },
-        tasks:{
-            type: Array,
-            required: true,
-            readiness: {
-                type: Boolean,
-                required: true
-            }
-        },
-        status: {
-            type: Number,
-            required: true
-        },
-    },
-
-    template: `
-        <div class="note">
-            <ul>
-                <li v-for="note in column_3">
-                    <p>{{ note.name }}</p>
-                    <ul>
-                        <li v-for="task in note.tasks" v-if="task.name !== null">
-                            <p>{{task.name}}</p>
-                            <input>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    `,
-
-    data(){
-        return{
-            column_3: [],
-            errors: [],
-            active: 0
-        }
-    },
-
-    mounted(){
-        this.column_3 = JSON.parse(localStorage.getItem("column_3")) || [];
-    },
-
-    watch: {
-        column_3(newValue) {
-            localStorage.setItem("column_3", JSON.stringify(newValue));
-        },
-    },
-
-    methods: {
         saveNote_3(){
             localStorage.setItem('column_3', JSON.stringify(this.column_3));
         },
     }
 
+})
 
 
+Vue.component('note', {
+    props: {
+        column: {
+            type: Array,
+            required: true,
+        },
+        errors: {
+            type: Array,
+            required: false,
+        }
+    },
+
+    template: `
+        <div>
+            <div class="note" >
+                <h1 v-for="error in errors"> {{ error }}</h1>
+                <ul>
+                    <li  v-for="note in column"  class="li-list">
+                        <h1>{{ note.name }}</h1>
+                        <p>{{ note }}</p>
+                        
+                        <ul>
+                            <li v-for="task in note.tasks" class="li-task" v-if="task !== null" >
+                                <p>{{task}}</p>
+                                <input type="checkbox" @click="updateStatus(note, task)">
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="note" >
+                <ul>
+                    <li v-for="note in column" class="li-list">
+                        <h1>{{ note.name }}</h1>
+                        <ul>
+                            <li v-for="task in note.tasks" class="li-task" v-if="task !== null" >
+                                <p>{{task}}</p>
+                                <input type="checkbox">
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="note" >
+                <ul>
+                    <li v-for="note in column" class="li-list">
+                        <h1>{{ note.name }}</h1>
+                        <ul>
+                            <li v-for="task in note.tasks" class="li-task" v-if="task !== null" >
+                                <p>{{task}}</p>
+                                <input type="checkbox">
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    `,
+
+    methods: {
+
+    }
 })
 
 Vue.component('create-note', {
@@ -208,6 +169,7 @@ Vue.component('create-note', {
             task_3: null,
             task_4: null,
             task_5: null,
+            id: 0,
             all_tasks: [],
             errors: []
         }
@@ -228,12 +190,11 @@ Vue.component('create-note', {
                         task_4: this.task_4,
                         task_5: this.task_5,
                     },
-                    data: null,
-                    сondition: 0,
+                    id:this.id,
+                    status: 0
                 }
-
+                this.idUp()
                 eventBus.$emit('notes-submitted', note);
-                eventBus.$emit('notes-poleteli', note);
                 this.name = null;
                 this.task_1 = null;
                 this.task_2 = null;
@@ -247,6 +208,9 @@ Vue.component('create-note', {
                 }
             }
         },
+        idUp(){
+            this.id++
+        }
     },
 })
 
