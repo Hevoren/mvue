41569b6b7cae454bd5bc563d1aae5e934e38b96d
fifau <fifaultimateamfifa15@gmail.com>
2023-03-14 23:53:33 +0700

    переход из одного столибика в другой сделан, теперь надо как то оформить сохранение текущей карты 'true' и 'false', чтобы они были заданы во втором столбце

--- js/main.js
+++ js/main.js
@@ -12,47 +12,41 @@ Vue.component('all-notes', {
 Vue.component('notes', {
     template: `
         <div class="notes">
-            <note :column="column_1" :errors="errors"></note>
-            <note :column="column_2" :errors="errors"></note>
-            <note :column="column_3" :errors="errors"></note>
+            <note :column="column_1" :errors="errors_1" :name="nameFirst" @changeTask="changeTask" ></note>
+            <note :column="column_2" :errors="errors_2" :name="nameSecond" @changeTask="changeTask"></note>
+            <note :column="column_3" :errors="errors_3" :name="nameThird"></note>
         </div>
     `,
 
     data() {
         return {
             column_1: [],
-            column_2: [
-                {
-                    name:'gg',
-                    tasks: [
-                        {name: 'adsasd', readiness: false},
-                        {name: 'asd', readiness: false},
-                        {name: 'sad', readiness: false},
-                        {name: 'asdasd', readiness: false},
-                        {name: 'asdsad', readiness: false},
-                    ],
-                    id: 5,
-                    status: 0
-                }
-            ],
+            column_2: [],
             column_3: [],
-            errors: []
+
+            errors_1: [],
+            errors_2: [],
+            errors_3: [],
+
+            nameFirst: 'Begin',
+            nameSecond: 'Continue',
+            nameThird: 'End',
+
+            changeTaskArr: [],
+
         }
     },
     // mounted - вызывается после того, как компонент был добавлен в DOM, т.е. србатывает после того как даннные улетели из формы сюда.
     // после чего он пытается достать и разобрать строку json из localstorage, и если ее нет, присваивает пустой массив
     mounted(){
-        console.log(this.column_2)
-        console.log(typeof (this.column_2))
         eventBus.$on('notes-submitted', note => {
-            this.errors = []
+            this.errors_1 = []
             if (this.column_1.length < 3){
                 this.column_1.push(note);
                 this.saveNote_1();
             } else {
-                this.errors.push('Maximum count of tasks!');
+                this.errors_1.push('GABELLA! Maximum count of tasks!');
             }
-
         })
     },
     // watch отслеживает изменения, если они есть, то он присваивает и сохраняет новые значения, добавляя их в localstorage и преобразовывая ('stringify') в json формат
@@ -78,6 +72,38 @@ Vue.component('notes', {
         saveNote_3(){
             localStorage.setItem('column_3', JSON.stringify(this.column_3));
         },
+        changeTask(task) {
+            let count = 0;
+            let countColumn_2 = 0;
+            this.column_1[task.indexNote].tasks[task.indexTask].readiness = !this.column_1[task.indexNote].tasks[task.indexTask].readiness;
+            const currentDateTime = new Date();
+
+            for(let i = 0; i < this.column_1[task.indexNote].tasks.length; i++){
+                if (this.column_1[task.indexNote].tasks[i].name !== null) {
+                    count++;
+                }
+            }
+            console.log("count", count)
+            for (let i = 0; i < this.column_1[task.indexNote].tasks.length; i++) {
+                if (this.column_1[task.indexNote].tasks[i].readiness === true) {
+                    countColumn_2 += 1;
+                }
+            }
+            console.log("countColumn_2", countColumn_2)
+            if (countColumn_2 / count * 100 >= 50 && this.column_2.length < 5) {
+                this.column_2.push(this.column_1[task.indexNote]);
+                this.column_1.splice(task.indexNote, 1);
+            }
+            if (countColumn_2 === count && this.column_3.length < 5) {
+                const note = this.column_2[task.indexNote];
+                note.datetime = currentDateTime;
+                this.column_3.push(note);
+                this.column_2.splice(task.indexNote, 1);
+            }
+            console.log(this.changeTaskArr)
+        },
+
+
     }
 
 })
@@ -92,22 +118,25 @@ Vue.component('note', {
         errors: {
             type: Array,
             required: true,
+        },
+        name: {
+            type: String,
+            required: true,
         }
     },
 
     template: `
         <div>
-            <div class="note" >
-                
+            <p>{{ name }}</p>
+            <div class="note">
                 <h1 v-for="error in errors"> {{ error }}</h1>
                 <ul>
-                    <li v-for="(note, index) in column" class="li-list">
+                    <li v-for="(note, indexNote) in column" class="li-list">
                         <h1>{{ note.name }}</h1>
                         <ul>
-                            <li v-for="(task, index) in note.tasks" class="li-task" v-if="task.name !== null" >
+                            <li v-for="(task, indexTask) in note.tasks" class="li-task" v-if="task.name !== null" >
                                 <p>{{ task.name }}</p>
-                                <input type="checkbox" v-if="note.id < 3" v-show="note.id < 3">
-                                <input type="checkbox" v-if="note.id > 3" v-show="note.id > 3">
+                                <input type="checkbox" @change="changeKeyTask(indexNote, indexTask, name)">
                             </li>
                         </ul>
                     </li>
@@ -117,6 +146,11 @@ Vue.component('note', {
     `,
 
     methods: {
+        changeKeyTask(indexNote, indexTask, name){
+            this.$emit('changeTask', {indexNote, indexTask, name})
+        },
+
+
     }
 })
 
@@ -152,6 +186,7 @@ Vue.component('create-note', {
 
     methods: {
         onSubmit() {
+            this.all_tasks = []
             this.errors = []
             this.all_tasks.push(this.task_1, this.task_2, this.task_3, this.task_4, this.task_5)
             this.all_tasks = this.all_tasks.filter(Boolean)
@@ -178,9 +213,7 @@ Vue.component('create-note', {
                 this.task_5 = null;
             }else {
                 this.all_tasks = []
-                if (this.errors.length === 0){
-                    this.errors.push("Gabella: from 3 to 5 tasks!")
-                }
+                this.errors.push("GABELLA! From 3 to 5 tasks!")
             }
         },
         idUp(){
