59d27b78c5b724368447d6a13a548100191ba854
hevoren <fifaultimateamfifa15@gmail.com>
2023-03-14 11:42:45 +0700

    full remake

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ Vue.component('note', {
             <div class="note" >
                 <h1 v-for="error in errors"> {{ error }}</h1>
                 <ul>
-                    <li  v-for="note in column"  class="li-list">
+                    <li v-for="note in column"  class="li-list">
                         <h1>{{ note.name }}</h1>
                         <p>{{ note }}</p>
                         
